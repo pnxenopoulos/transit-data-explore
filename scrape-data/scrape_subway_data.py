167877a9c23bbba8c3f6feffd2aab96c06106b8a
Peter Xenopoulos <pnxenopoulos@gmail.com>
2019-04-04 10:32:03 -0400

    added mta turnstile scrape script

--- scrape-data/scrape_subway_data.py
+++ scrape-data/scrape_subway_data.py
@@ -10,26 +10,12 @@ url = 'http://web.mta.info/developers/turnstile.html'
 page_html = readHTMLPage(url)
 
 # Find all links to CSVs and remove last one, which isn't data we need
-all_links = re.findall('\".*\.txt\"', page_html)
-data_links = all_links[:-1]
-
-# add links to yellow cab, green cab and fhv lists
-yellow, green, fhv = [], [], []
-for link in data_links:
-	link = link.replace('\"', '')
-	if bool(re.search('yellow', link)):
-		yellow.append(link)
-	elif bool(re.search('green', link)):
-		green.append(link)
-	else:
-		fhv.append(link)
+all_links = re.findall('data/nyct/turnstile/turnstile_\d{6}\.txt', page_html)
+url_start = 'http://web.mta.info/developers/'
+data_links = [url_start + s for s in all_links]
 
 # Separate and concatenate the data
-yellow_cab_data = concatDF(yellow)
-green_cab_data = concatDF(green)
-fhv_data = concatDF(fhv)
+mta_turnstile_data = concatDF(data_links)
 
 # Write to data folder
-yellow_cab_data.to_csv('data/yellow_cab_data.csv', index = False)
-green_cab_data.to_csv('data/green_cab_data.csv', index = False)
-fhv_data.to_csv('data/fhv_data.csv', index = False)
+mta_turnstile_data.to_csv('data/mta_turnstile.csv', index = False)
