30f867fb92107d4c5cfa588b86c52fccadc4e0c7
Peter Xenopoulos <pnxenopoulos@gmail.com>
2019-04-08 17:06:00 -0400

    minor code changes

--- scrape-data/scrape_subway_data.py
+++ scrape-data/scrape_subway_data.py
@@ -14,12 +14,13 @@ all_links = re.findall('data/nyct/turnstile/turnstile_\d{6}\.txt', page_html)
 url_start = 'http://web.mta.info/developers/'
 data_links = [url_start + s for s in all_links]
 
-mta_turnstile_data = pd.DataFrame()
-mta_turnstile_data.columns = ['ca', ]
+mta_turnstile_data = pd.DataFrame(columns = ['ca', 'unit', 'scp', 'station', 'linename', 'division', 'date', 'time', 'desc', 'entries', 'exists'])
+
 # Separate and concatenate the data
 for link in data_links:
 	mta_turnstile_data = concatDF(mta_turnstile_data, link)
 
 mta_turnstile_data.columns = ['ca', 'unit', 'scp', 'station', 'linename', 'division', 'date', 'time', 'desc', 'entries', 'exists']
+
 # Write to data folder
 mta_turnstile_data.to_csv('data/mta_turnstile.csv', index = False)
